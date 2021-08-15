cdba90db9e9c4edc4ff1c5c519ce9db6954dd31b
Jackson Buzzano <jcksb@hotmail.com>
2021-08-15 08:50:32 -0300

    Finalizando a busca do paciente pela API

--- js/buscar_paciente.js
+++ js/buscar_paciente.js
@@ -1,35 +1,32 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
+const erro_busca_pac = document.getElementById("alert");
+const erroAjax = document.getElementById("alerta");
+const filtar_api = document.getElementById("filtrar-api");
 
 botaoAdicionar.addEventListener("click",function(){
     const resultado_api = new XMLHttpRequest();
     resultado_api.open("GET","http://localhost/Curso%20OnLine/Projeto%2001/Api.txt", true);
 
     resultado_api.addEventListener("load",function(){
-        const erroAjax = document.querySelector("#erro-ajax");
-        const filtar_api = document.getElementById('filtrar-api');
-        
+
         if(resultado_api.status == 200){
-            erroAjax.classList.add("invisivel");
             var resposta = resultado_api.responseText ;
             
             const pacientes = JSON.parse(resposta);
 
                 for(var  i = 0; i < pacientes.length; i++){
-                    if(filtar_api.value === pacientes[i].nome){
+                    if(filtar_api.value.toUpperCase() === pacientes[i].nome.toUpperCase()){
                         adicionaPacienteNaTabela(pacientes[i]);
                         filtar_api.value = "";
-                        erroAjax.style.display = "none";
+                        erro_busca_pac.style.display = "none";
                         break;
                     }else{
-                        erroAjax.style.display = "block";
+                        erro_busca_pac.style.display = "block";
                     }
                 };
 
         }else{
-            console.log(resultado_api.status);
-            console.log(resultado_api.responseText);     
-
-            erroAjax.classList.remove("invisivel")
+            erroAjax.style.display = "block";
         }
     });
  
