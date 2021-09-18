aef95c0ec0f6b93287720e91d5100cec5dcf6fe9
Jackson Buzzano <jcksb@hotmail.com>
2021-09-18 10:26:07 -0300

    Realizando ajustes no estilo e criando um modal

--- publico/javaScript/javascritp.js
+++ publico/javaScript/javascritp.js
@@ -1,14 +1,5 @@
-function voltarPagina() {
-    swal({
-      title: "Atenção!",
-      text: "Deseja sair sem salvar?!",
-      icon: "warning",
-      buttons: true,
-    }).then(function(result) {
-      if (result) {
-        window.location.href="http://localhost:8080/form-pacient";
-      } else {
-        //window.location.href="http://localhost:8080/form-pacient/cad-paciente";
-      }
-    });
+  //função pra aparecer a opção de carregando
+  function carregarSpinners(){
+    const text_center = document.getElementById('text-center');
+    text_center.style.display = 'block';
   }
