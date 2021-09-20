9109ccf8770457d9d5dbabea0ea87593c90045b1
Jackson Buzzano <jcksb@hotmail.com>
2021-09-20 19:12:08 -0300

    Fazendo ajustes

--- publico/javaScript/javascritp.js
+++ publico/javaScript/javascritp.js
@@ -1,11 +1,11 @@
 //função pra aparecer a opção de carregando
-function carregarSpinners(){
-  const text_center = document.getElementById('text-center');
-  text_center.style.display = 'block';
+function carregarSpinners() {
+  const text_center = document.getElementById("text-center");
+  text_center.style.display = "block";
 }
 //FORMATAÇÃO DO TELEFONE
 function mask(o, f) {
-  setTimeout(function() {
+  setTimeout(function () {
     var v = mphone(o.value);
     if (v != o.value) {
       o.value = v;
@@ -30,31 +30,73 @@ function mphone(v) {
 //FORMATAÇÃO DO EMAIL
 function verifica() {
   if (document.forms[0].email.value.length == 0) {
-    alert('Por favor, informe o seu EMAIL.');
-  document.frmEnvia.email.focus();
+    alert("Por favor, informe o seu EMAIL.");
+    document.frmEnvia.email.focus();
     return false;
-  }else{
+  } else {
     return true;
   }
-  
 }
-   
-function checarEmail(){
-if( document.forms[0].email.value=="" 
-   || document.forms[0].email.value.indexOf('@')==-1 
-     || document.forms[0].email.value.indexOf('.')==-1 )
-  {
-     alert( "Por favor, informe um E-MAIL válido!" );
-     return false;
-     
+
+function checarEmail() {
+  if (
+    document.forms[0].email.value == "" ||
+    document.forms[0].email.value.indexOf("@") == -1 ||
+    document.forms[0].email.value.indexOf(".") == -1
+  ) {
+    alert("Por favor, informe um E-MAIL válido!");
+    return false;
   }
 }
 
 /*CASO FOR PAGINA DE LOGIN NÃO MOSTRAR O MENU*/
 const url = window.location.pathname;
 
-if(url === "/login-page"){
-  const menu = document.getElementById('menu');
+if (url === "/login-page") {
+  const menu = document.getElementById("menu");
   menu.style.display = "none";
 }
 
+/*CALCULAR IDADE SOZINHO*/
+document.getElementById("data").addEventListener("change", function () {
+  var data = new Date(this.value);
+  if (isDate_(this.value) && data.getFullYear() > 1900)
+    document.getElementById("idade").value = calculaIdade(this.value);
+});
+
+function calculaIdade(dobString) {
+  var dob = new Date(dobString);
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var birthdayThisYear = new Date(currentYear, dob.getMonth(), dob.getDate());
+  var age = currentYear - dob.getFullYear();
+  if (birthdayThisYear > currentDate) {
+    age--;
+  }
+  console.log(age);
+  return age;
+}
+
+function calcular(data) {
+  var data = document.form.nascimento.value;
+  alert(data);
+  var partes = data.split("/");
+  var junta = partes[2] + "-" + partes[1] + "-" + partes[0];
+  document.form.idade.value = calculaIdade(junta);
+}
+
+var isDate_ = function (input) {
+  var status = false;
+  if (!input || input.length <= 0) {
+    status = false;
+  } else {
+    var result = new Date(input);
+    if (result == "Invalid Date") {
+      status = false;
+    } else {
+      status = true;
+    }
+  }
+  console.log(status);
+  return status;
+};
