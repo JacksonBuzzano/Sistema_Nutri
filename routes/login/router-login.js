e70b96b394801b3d130982514aa8bb0d01337e72
Jackson Buzzano <jcksb@hotmail.com>
2021-09-25 11:13:48 -0300

    Fazendo ajustes

--- routes/login/router-login.js
+++ routes/login/router-login.js
@@ -4,23 +4,23 @@ const router = express.Router();
 const db = require("../../models/SQLPassword/login");
 
 //Rotas
-router.get('/', function(req, res) {
-    res.render('login-page/login');
+router.get("/", function (req, res) {
+  res.render("login-page/login");
 });
 
-router.post('/logar', function(req, res) {
-    (async () =>{
-        const nome = req.body.nome;
-        const senha = req.body.senha;
-    
-        const dados = await db.verificaSenha();
-       if(dados.find((user, index, array) => user.nm_nome === nome && user.nr_senha === senha)){
-           res.redirect('/form-agenda')
-       }else{
-           res.redirect('/login-page')
-       }
+router.post("/logar", function (req, res) {
+  (async () => {
+    const nome = req.body.nome;
+    const senha = req.body.senha;
 
-    })();
-})
+    const dados = await db.verificaSenha();
+    if (dados.find((user, index, array) => user.nm_nome === nome && user.nr_senha === senha)) {
+        res.redirect("/form-agenda");
+    } else {
+      res.redirect("/login-page");
+    }
+
+  })();
+});
 
 module.exports = router;
